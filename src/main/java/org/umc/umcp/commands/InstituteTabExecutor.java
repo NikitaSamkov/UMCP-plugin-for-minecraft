cca442134566fc61e9797b555ef1e457907e3b91
NikitaSamkov <nick.samkov@yandex.ru>
2022-02-26 02:00:33 +0500

    added colors!!!!

--- src/main/java/org/umc/umcp/commands/InstituteTabExecutor.java
+++ src/main/java/org/umc/umcp/commands/InstituteTabExecutor.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.bukkit.entity.Player;
 import org.umc.umcp.connection.DBConnection;
@@ -23,10 +24,13 @@ public class InstituteTabExecutor implements TabExecutor {
     private final DBConnection conn;
     private final List<String> institutes;
     private final Map<String, InstituteSubcommand> subcommands;
+    private final Map<String, String> painter;
 
     public InstituteTabExecutor() {
         conn = new DBConnection("jdbc:mysql://umcraft.scalacubes.org:2163/UMCraft", "root", "4o168PPYSIdyjFU");
         institutes = GetInstitutes();
+        painter = CreatePainter(institutes);
+
         subcommands = new HashMap<String, InstituteSubcommand>();
         subcommands.put("join", this::Join);
         subcommands.put("info", this::Info);
@@ -36,7 +40,7 @@ public class InstituteTabExecutor implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 1)
-            return false;
+            return InstitutesList(sender);
         return subcommands.get(args[0].toLowerCase(Locale.ROOT)).subcommand(sender, command, label, args);
     }
 
@@ -51,13 +55,18 @@ public class InstituteTabExecutor implements TabExecutor {
         return null;
     }
 
+    private boolean InstitutesList(CommandSender sender) {
+        sender.sendMessage(painter.values().stream().sorted().collect(Collectors.joining("\n")));
+        return true;
+    }
+
     private boolean Join(CommandSender sender, Command command, String label, String[] args) {
         String instituteName = args[1];
         if (!(sender instanceof Player)) return false;
         Player player = (Player) sender;
         player.getUniqueId();
         if (JoinInstitute(player.getUniqueId().toString(), instituteName)) {
-            sender.sendMessage("Успешно сменен институт на " + instituteName + "!");
+            sender.sendMessage("Успешно сменен институт на " + painter.get(instituteName) + "!");
             return true;
         } else {
             sender.sendMessage("Произошла ошибка: проверьте правильность написания названия института (даже подсказки есть)");
@@ -72,7 +81,7 @@ public class InstituteTabExecutor implements TabExecutor {
             ResultSet result = conn.MakeQuery(String.format("select id, name from institutes " +
                     "inner join players p on institutes.id = p.institute where p.uuid='%s'", player.getUniqueId()));
             if (result.next()) {
-                sender.sendMessage(String.format("Вы сейчас состоите в институте %s!", result.getString("name")));
+                sender.sendMessage(String.format("Вы сейчас состоите в институте %s!", painter.get(result.getString("name"))));
             } else {
                 sender.sendMessage("Вы сейчас не состоите ни в каком институте, поступите в него командой \"/institute join <название института>\"!");
             }
@@ -123,6 +132,24 @@ public class InstituteTabExecutor implements TabExecutor {
         }
         conn.Close();
 
+        return result;
+    }
+
+    private List<String> GetColorfulElements(List<String> list) {
+        List<String> paints = Arrays.asList("2", "3", "4", "5", "6", "7", "9", "a", "b", "c", "d", "e", "f");
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            result.add("§" + paints.get(i % paints.size()) + list.get(i) + "§f");
+        }
+        return result;
+    }
+
+    private Map<String, String> CreatePainter(List<String> list) {
+        List<String> colored = GetColorfulElements(list);
+        Map<String, String> result = new HashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            result.put(list.get(i), colored.get(i));
+        }
         return result;
     }
 }
