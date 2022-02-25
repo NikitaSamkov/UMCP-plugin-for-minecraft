acb4e3bf70976f7abf1815cd50f3e17b0cc2ee7a
NikitaSamkov <nick.samkov@yandex.ru>
2022-02-25 19:36:51 +0500

    added connection to database

--- src/main/java/org/umc/umcp/MyExecutor.java
+++ src/main/java/org/umc/umcp/MyExecutor.java
@@ -3,14 +3,31 @@ package org.umc.umcp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MyExecutor implements CommandExecutor
+public class MyExecutor implements TabExecutor
 {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         sender.sendMessage("Test");
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            List<String> result = new ArrayList<>();
+            result.add("Test2");
+            result.add("Test3");
+            result.add("Test4");
+            return result;
+        }
+
+        return null;
     }
 }
---
+++ src/main/java/org/umc/umcp/connection/DBConnection.java
@@ -0,0 +1,46 @@
+package org.umc.umcp.connection;
+
+import java.sql.*;
+
+public class DBConnection {
+    private final String url;
+    private final String login;
+    private final String pass;
+
+    private Connection conn;
+    private Statement stmt;
+
+    public DBConnection(String url, String login, String password){
+        this.url = url == null ? "" : url;
+        this.login = login == null ? "" : login;
+        this.pass = password == null ? "" : password;
+    }
+
+    public void Connect() {
+        try {
+            conn = DriverManager.getConnection(url, login, pass);
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Close() {
+        try {
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet MakeQuery(String query) {
+        try {
+            ResultSet result = stmt.executeQuery(query);
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
