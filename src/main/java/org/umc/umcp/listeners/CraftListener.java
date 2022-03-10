2a22760ac99a07a05d072449becd1f40a8189ff4
NikitaSamkov <nick.samkov@yandex.ru>
2022-03-10 14:41:50 +0500

    random colored vape!!!!

--- src/main/java/org/umc/umcp/listeners/CraftListener.java
+++ src/main/java/org/umc/umcp/listeners/CraftListener.java
@@ -6,11 +6,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.umc.umcp.Crafter;
 import org.umc.umcp.Main;
 import org.umc.umcp.connection.DBConnection;
 
+import java.awt.*;
+import org.bukkit.Color;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class CraftListener implements Listener {
     @EventHandler
@@ -20,8 +26,14 @@ public class CraftListener implements Listener {
         }
         Player player = (Player) e.getViewers().get(0);
         String institute = Main.conn.GetInstitute(player.getUniqueId().toString());
-        if (e.getRecipe().getResult().equals(new ItemStack(Material.DIAMOND)) && !institute.equals("ИРИТ-РТФ")) {
+        if (e.getRecipe().getResult().equals(Crafter.Vape) && !institute.equals("ИРИТ-РТФ")) {
             e.getInventory().setResult(new ItemStack(Material.AIR));
+        } else {
+            PotionMeta vapeMeta = (PotionMeta) Crafter.Vape.getItemMeta();
+            Random r = new Random();
+            vapeMeta.setColor(Color.fromRGB(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+            Crafter.Vape.setItemMeta(vapeMeta);
+            e.getInventory().setResult(Crafter.Vape);
         }
     }
 }
