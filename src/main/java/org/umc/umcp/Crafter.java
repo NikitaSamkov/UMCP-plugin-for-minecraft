faf8bd736fafd2b7fe263cb0e0d01ac7c4f0b83d
NikitaSamkov <nick.samkov@yandex.ru>
2022-03-10 21:19:05 +0500

    added enum UmcpItem

--- src/main/java/org/umc/umcp/Crafter.java
+++ src/main/java/org/umc/umcp/Crafter.java
@@ -3,8 +3,6 @@ package org.umc.umcp;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -31,16 +29,14 @@ public class Crafter {
 
     public static void CreateItems() {
         //Вейп
-        Vape = new ItemStack(Material.POTION, 1);
+        Vape = new ItemStack(UmcpItem.VAPE.getMaterial(), 1);
         PotionMeta vapeMeta = (PotionMeta) Vape.getItemMeta();
-        vapeMeta.setDisplayName("Вейп");
-        List<String> lore = new ArrayList<>();
-        lore.add("Одноразовый вейп радиста.");
-        lore.add("Слишком частое употребление вредно для здоровья!");
-        vapeMeta.setLore(lore);
-        vapeMeta.setCustomModelData(1337);
+        vapeMeta.setDisplayName(UmcpItem.VAPE.getDisplayName());
+        vapeMeta.setLore(UmcpItem.VAPE.getLore());
+        vapeMeta.setCustomModelData(UmcpItem.VAPE.getCustomModelData());
         vapeMeta.setColor(Color.fromRGB(255, 255, 255));
         vapeMeta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 0, false, false, true), true);
         Vape.setItemMeta(vapeMeta);
     }
 }
+
---
+++ src/main/java/org/umc/umcp/UmcpItem.java
@@ -0,0 +1,45 @@
+package org.umc.umcp;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum UmcpItem {
+    VAPE("Вейп", Arrays.asList("Одноразовый вейп радиста.", "Слишком частое употребление вредно для здоровья!"), Material.POTION, 1337);
+
+    private final String displayName;
+    private final List<String> lore;
+    private final Material material;
+    private final int customModelData;
+
+
+    private UmcpItem(String displayName, List<String> lore, Material material, int customModelData) {
+        this.displayName = displayName;
+        this.lore = lore;
+        this.material = material;
+        this.customModelData = customModelData;
+    }
+
+    public static Boolean check(@NotNull ItemStack itemStack, @NotNull UmcpItem umcpItem) {
+        return (itemStack.getType().equals(umcpItem.getMaterial()) && itemStack.getItemMeta().getCustomModelData() == umcpItem.getCustomModelData());
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public int getCustomModelData() {
+        return customModelData;
+    }
+
+    public List<String> getLore() {
+        return lore;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
