c909da8ff8b56a31beb2ac1b489b6a47e1a3641f
NikitaSamkov <nick.samkov@yandex.ru>
2022-03-22 01:44:43 +0500

    items to config

--- src/main/java/org/umc/umcp/enums/UmcpItem.java
+++ src/main/java/org/umc/umcp/enums/UmcpItem.java
@@ -3,21 +3,22 @@ package org.umc.umcp.enums;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.umc.umcp.Main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public enum UmcpItem {
-    VAPE("Вейп", Arrays.asList("Одноразовый вейп радиста.", "Слишком частое употребление вредно для здоровья!"), Material.POTION, 1337),
-    SOCKS("Чулки радиста", new ArrayList<>(), Material.GOLDEN_BOOTS, 1),
-    LONGSOCKS("Чулки радиста", new ArrayList<>(), Material.GOLDEN_LEGGINGS, 1),
-    CAT_EARS("Кошачьи ушки", new ArrayList<>(), Material.LEATHER_HELMET, 1),
+    VAPE("rtf.items.vape.Name", "rtf.items.vape.Desc", Material.POTION, "rtf.items.vape.ModelData"),
+    SOCKS("rtf.items.socks.Name", "rtf.items.socks.Desc", Material.GOLDEN_BOOTS, "rtf.items.socks.ModelData"),
+    LONGSOCKS("rtf.items.longsocks.Name", "rtf.items.longsocks.Desc", Material.GOLDEN_LEGGINGS, "rtf.items.longsocks.ModelData"),
+    CAT_EARS("rtf.items.catears.Name", "rtf.items.catears.Desc", Material.LEATHER_HELMET, "rtf.items.catears.ModelData"),
 
-    SPORT_HELMET("Козырёк Абибас", new ArrayList<>(), Material.DIAMOND_HELMET, 1),
-    SPORT_CHESTPLATE("Куртка Адик даст", new ArrayList<>(), Material.DIAMOND_CHESTPLATE, 1),
-    SPORT_LEGGINGS("Спортивные штаны Бабидас", new ArrayList<>(), Material.DIAMOND_LEGGINGS, 1),
-    SPORT_BOOTS("Фирменные кроссовки Адибас", new ArrayList<>(), Material.DIAMOND_BOOTS, 1);
+    SPORT_HELMET("ifksimp.items.sporthelmet.Name", "ifksimp.items.sporthelmet.Desc", Material.DIAMOND_HELMET, "ifksimp.items.sporthelmet.ModelData"),
+    SPORT_CHESTPLATE("ifksimp.items.sportchestplate.Name", "ifksimp.items.sportchestplate.Desc", Material.DIAMOND_CHESTPLATE, "ifksimp.items.sportchestplate.ModelData"),
+    SPORT_LEGGINGS("ifksimp.items.sportleggings.Name", "ifksimp.items.sportleggings.Desc", Material.DIAMOND_LEGGINGS, "ifksimp.items.sportleggings.ModelData"),
+    SPORT_BOOTS("ifksimp.items.sportboots.Name", "ifksimp.items.sportboots.Desc", Material.DIAMOND_BOOTS, "ifksimp.items.sportboots.ModelData");
 
     private final String displayName;
     private final List<String> lore;
@@ -30,6 +31,10 @@ public enum UmcpItem {
         this.lore = lore;
         this.material = material;
         this.customModelData = customModelData;
+    }
+
+    private UmcpItem(String namePath, String lorePath, Material material, String cmdPath) {
+        this(Main.config.getString(namePath), Main.config.getStringList(lorePath), material, Main.config.getInt(cmdPath));
     }
 
     public static Boolean check(@NotNull ItemStack itemStack, @NotNull UmcpItem umcpItem) {
