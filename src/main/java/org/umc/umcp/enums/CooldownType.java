f886ae8a0570b25a937d887544eee87967b6104d
NikitaSamkov <nick.samkov@yandex.ru>
2022-03-25 12:34:20 +0500

    cooldowns to config

--- src/main/java/org/umc/umcp/enums/CooldownType.java
+++ src/main/java/org/umc/umcp/enums/CooldownType.java
@@ -1,10 +1,13 @@
 package org.umc.umcp.enums;
 
+import org.jetbrains.annotations.Nullable;
+import org.umc.umcp.Main;
+
 import java.util.Date;
 
 public enum CooldownType {
-    VAPE(0, 0, 1, 0, 0),
-    INSTITUTE_JOIN(2, 0, 0, 0, 0);
+    VAPE(Main.config.getString("cooldowns.vape")),
+    INSTITUTE_JOIN(Main.config.getString("cooldowns.join"));
 
     public Date time;
 
@@ -18,5 +21,14 @@ public enum CooldownType {
 
     private CooldownType(int days, int hours, int minutes, int seconds, int milliseconds) {
         this.time = new Date((((days * 24L + hours) * 60L + minutes) * 60L + seconds) * 1000L + milliseconds);
+    }
+
+    private CooldownType(@Nullable String s) {
+        if (s == null) {
+            s = "0:0:0:0:0";
+        }
+        String[] params = s.split(":");
+        this.time = new Date((((Integer.parseInt(params[0]) * 24L + Integer.parseInt(params[1])) * 60L +
+                Integer.parseInt(params[2])) * 60L + Integer.parseInt(params[3])) * 1000L + Integer.parseInt(params[4]));
     }
 }
